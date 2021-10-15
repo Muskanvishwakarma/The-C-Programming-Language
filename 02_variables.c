0d5d10d6220e276a21d5cdabb86b71f53dbe8c84
Muskanvishwakarma <92566858+Muskanvishwakarma@users.noreply.github.com>
2021-10-15 11:39:03 +0530

    Update 02_variables.c

--- 02_variables.c
+++ 02_variables.c
@@ -9,7 +9,7 @@ int main()
     printf("the value of a is %d \n", a);
     printf("the value of b is %d \n", b);
     printf("the value of c is %d \n", c);
-    printf("the sum of a and b and c is %d \n", a+b+c);
+    printf("the sum of a and b and c is %f \n", a+b+c);
 
     return 0;
 }
